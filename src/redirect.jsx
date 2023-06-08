34ff993bdef5163be3b7e2c9435aee7cbf67bf36
KhushnudEshtemirov <khushnudeshtemirov@gmail.com>
2023-06-08 05:21:26 +0500

    Finished

--- src/redirect.jsx
+++ src/redirect.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import axios from "axios";
 import { useLocation } from "react-router-dom";
 
 const Redirect = () => {
   const location = useLocation();
 
-  //https://login-goole.vercel.app/redirect/facebook?code=AQCrfLSN_uEv1UWLoBvkdqMY5_AtbliJfsyH3kA59mOs1RFEfKCppUlT3g6eE3pQWtdf3yDP_BTgU3stIrBndodnxby7GOKfJ9tPsGoKzn3mnarVjMuUZuPQPCc3G-bBNgFuh3r2402TRjBEnK3qwABAp2jUuOLIDL_gFb1eXaWI0SBC7Yj7x_5d-ttOyNXWyP7Sy4T86wzweRYWQz8BfgfrHzCT8SsBg1NOfQguQ9R4-8n_op6zig8KcssEgGZHKgkYMtx0Ermb5ze4hzOy_MG2bZEisBuXCnUQ5O8kKedoU0yDye8hWugOgknXXFB51MSlSJajya1dk0h3tALUjD9OH8lUBRlspiaC1-CHRco-V8lRBTrKRXth1RwvBh2SQ0w#_=_
+  console.log(location.search.slice(7));
 
-  console.log(location);
+  const url = location.search.slice(7);
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const user = await axios
+          .get(
+            `https://api.vatandoshlarfondi.uz/api/oauth/call-back/facebook?code=${url}`
+          )
+          .then((res) => res.data);
+
+        console.log(user);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    getUser();
+  }, []);
 
   return <div>hwqugduiqwgdiuqwg</div>;
 };
